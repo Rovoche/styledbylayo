531e6114c0790aa30d00bef71589ecfcec5bfa4b
Sammie E Friday <sammietwyne@gmail.com>
2026-06-21 15:59:03 +0100

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind({
-    nested: true,
-  })],
+  site: 'https://rovoche.github.io',
+  base: '/styledbylayo/',
   server: {
     port: 3000,
     host: true,
